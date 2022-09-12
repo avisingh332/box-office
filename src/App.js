2bd7f28f961437fc18195affd69d6cae85517ac6
Avinash Singh <avisingh332@gmail.com>
2022-09-12 09:32:51 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+
 function App() {
-  return <div className="App">Hi</div>;
+  // return <div className="App">Hi</div>;
+  return (
+    <Switch>
+      <Route path="/first"> This is my page</Route>
+      <Route path="/second">This is my second page</Route>
+      <Route>This is Home page</Route>
+    </Switch>
+  );
 }
 
 export default App;
