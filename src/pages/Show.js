3005a9c5191c5c6ed1c3090173b84fe648f2bd82
Avinash Singh <avisingh332@gmail.com>
2022-09-23 00:13:02 +0530

    styled show page and and all relative components

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,12 @@
+/* eslint no-underscore-dangle: 0 */
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+import Cast from '../components/shows/Cast';
+import Details from '../components/shows/Details';
+import Seasons from '../components/shows/Seasons';
+import ShowMainData from '../components/shows/ShowMainData';
 import { apiGet } from '../misc/config';
+import { InfoBlock, ShowPageWrapper } from './Show.styled';
 
 const initialState = {
   show: null,
@@ -18,12 +24,16 @@ function reduce(prevState, action) {
   }
 }
 const Show = () => {
-  const [state, dispatch] = useReducer(reduce, initialState);
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reduce,
+    initialState
+  );
   const { id } = useParams();
   // const [show, setShow] = useState(null);
   // const [isLoading, setIsLoading] = useState(true);
   // const [error, setError] = useState(null);
   useEffect(() => {
+    // https://api.tvmaze.com/shows/1?embed[]=episodes&embed[]=cast
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(result => {
         // setShow(result);
@@ -37,13 +47,42 @@ const Show = () => {
       });
   }, [id]);
 
-  if (state.isLoading) {
+  if (isLoading) {
     return <div>Data is loading </div>;
   }
-  if (state.error) {
-    return <div>Error occured {state.error}</div>;
+  if (error) {
+    return <div>Error occured {error}</div>;
   }
 
-  return <div>{console.log('Show', state.show)}This is show page</div>;
+  return (
+    <ShowPageWrapper>
+      {console.log(show)}
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+      <InfoBlock>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          premiered={show.premiered}
+          network={show.network}
+        />
+      </InfoBlock>
+
+      <InfoBlock>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </InfoBlock>
+
+      <InfoBlock>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </InfoBlock>
+    </ShowPageWrapper>
+  );
 };
 export default Show;
