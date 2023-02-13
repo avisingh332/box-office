d6941f14d86adffb27c9d7cce00e7913aaa0e6c6
Avinash Singh <avisingh332@gmail.com>
2023-02-13 20:24:59 +0530

    Updates

---
+++ precache-manifest.aa0e7dbd3c748d8203e8e1ac8e167cfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1eccedf3572121686e34",
+    "url": "/box-office/static/css/main.51c23b49.chunk.css"
+  },
+  {
+    "revision": "1eccedf3572121686e34",
+    "url": "/box-office/static/js/main.1eccedf3.chunk.js"
+  },
+  {
+    "revision": "0f42ad21f20e3536abc0",
+    "url": "/box-office/static/js/1.0f42ad21.chunk.js"
+  },
+  {
+    "revision": "815fff4963b99e5a0bd5",
+    "url": "/box-office/static/js/2.815fff49.chunk.js"
+  },
+  {
+    "revision": "67be8d3dd68d83d02106",
+    "url": "/box-office/static/js/runtime~main.67be8d3d.js"
+  },
+  {
+    "revision": "c5b339ff29c1a53bcee4481f5d47d422",
+    "url": "/box-office/index.html"
+  }
+];
