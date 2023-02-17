e20ee954f78bfd4982828fb3505220d4c846ab4d
Avinash Singh <avisingh332@gmail.com>
2023-02-17 23:49:57 +0530

    Added Service Worker file and added PWA(Progressive Web App) Capabilities

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
-
+import * as serviceWorker from './serviceWorker';
 const root = ReactDOM.createRoot(document.getElementById('root')); //
 root.render(
   <HashRouter>
@@ -13,6 +13,7 @@ root.render(
   </HashRouter>
 );
 
+serviceWorker.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
